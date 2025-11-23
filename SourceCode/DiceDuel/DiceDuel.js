ede24b52a9b2927268514e686504edfd0a1df3fc
chara1 <saket.nimkar@gmail.com>
2025-11-23 20:34:29 +0000

    Update
    dice changes numbers and is displayed, added a timer to Quick Draw

--- SourceCode/DiceDuel/DiceDuel.js
+++ SourceCode/DiceDuel/DiceDuel.js
@@ -1,8 +1,20 @@
-console.log("External JS loaded!");
-
 // Global variables to store the dice rolls
 let num1 = null;
 let num2 = null;
+
+// update so it looks like you're rolling dice 
+
+let rolling1 = setInterval(() => {
+    document.getElementById("Number_output1").innerHTML =
+        Math.floor(Math.random() * 6) + 1;
+}, 100); // s 0.1
+
+let rolling2 = setInterval(() => {
+    document.getElementById("Number_output2").innerHTML =
+        Math.floor(Math.random() * 6) + 1;
+}, 100);
+    
+// to roll the dice's
 
 // player 1
 document.getElementById("roll_button1").onclick = function () {
@@ -10,9 +22,9 @@ document.getElementById("roll_button1").onclick = function () {
     num1 = Math.floor(Math.random() * 6) + 1;
     // Display results
     document.getElementById("Number_output1").innerHTML = num1;
-        // Disable the button completely
+    // Disable the button completely
     document.getElementById("roll_button1").disabled = true;
-
+    clearInterval(rolling1);
     checkWinner();
 };
 
@@ -21,26 +33,25 @@ document.getElementById("roll_button2").onclick = function () {
     num2 = Math.floor(Math.random() * 6) + 1;
     document.getElementById("Number_output2").innerHTML = num2;
     document.getElementById("roll_button2").disabled = true;
-
+    clearInterval(rolling2);
     checkWinner();
 };
 
 // Function to check the winner
+
 function checkWinner() {
     if (num1 !== null && num2 !== null) { // Only run when both players have rolled
         if (num1 > num2) {
-            document.getElementById("lls").innerHTML = "Player 1 wins!";
+            document.getElementById("results").innerHTML = "Player 1 wins!";
         } else if (num2 > num1) {
-            document.getElementById("lls").innerHTML = "Player 2 wins!";
+            document.getElementById("results").innerHTML = "Player 2 wins!";
         } else {
-            document.getElementById("lls").innerHTML = "It's a draw! Try again.";
+            document.getElementById("results").innerHTML = "It's a draw! Try again.";
         }
     }
 }
 
-
 document.getElementById("reset_button").onclick = function () {
     location.reload();
-
 };
 
--- SourceCode/QuickDraw/QuickDraw.js
+++ SourceCode/QuickDraw/QuickDraw.js
@@ -1,16 +1,35 @@
 console.log("External JS loaded!");
 
+// disabled button 
+document.getElementById("roll_Draw2").disabled = true;
+document.getElementById("roll_Draw1").disabled = true;
+
+// count down
+var counter = 4;
+var interval = setInterval(function() {
+    counter--;
+    // Display 'counter' wherever you want to display it.
+    if (counter <= -1) {
+     		clearInterval(interval);
+        document.getElementById("time").innerHTML = "Go!";
+        document.getElementById("roll_Draw2").disabled = false;
+        document.getElementById("roll_Draw1").disabled = false;
+        return;
+    }else{
+      console.log("Timer --> " + counter);
+      document.getElementById("time").innerHTML = counter + " Seconds";
+    }
+}, 1000);
+
 // player 1
 document.getElementById("roll_Draw1").onclick = function () {
 
     document.getElementById("lls").innerHTML = "Player 1 wins!";
 
         // Disable the button completely
+    document.getElementById("time").innerHTML = "";
     document.getElementById("roll_Draw1").disabled = true;
     document.getElementById("roll_Draw2").disabled = true;
-
-
-    checkWinner();
 };
 
 // player 2
@@ -18,30 +37,11 @@ document.getElementById("roll_Draw2").onclick = function () {
 
     document.getElementById("lls").innerHTML = "Player 2 wins!";
 
-
+    document.getElementById("time").innerHTML = "";
     document.getElementById("roll_Draw2").disabled = true;
     document.getElementById("roll_Draw1").disabled = true;
-
-
-    checkWinner();
 };
-
-// // Function to check the winner
-// function checkWinner() {
-//     if (num1 !== null && num2 !== null) { // Only run when both players have rolled
-//         if (num1 > num2) {
-//             document.getElementById("lls").innerHTML = "Player 1 wins!";
-//         } else if (num2 > num1) {
-//             document.getElementById("lls").innerHTML = "Player 2 wins!";
-//         } else {
-//             document.getElementById("lls").innerHTML = "It's a draw! Try again.";
-//         }
-//     }
-// }
-
 
 document.getElementById("reset_button").onclick = function () {
     location.reload();
-
 };
-
